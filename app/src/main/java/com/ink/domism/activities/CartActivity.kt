598413501d7742e718f5fc4f564a2350a3f5d113
mNik033 <nktps40@gmail.com>
2023-07-23 16:57:38 +0530

    add modal bottom sheet for payment flow
    minor improvements in card view layout
    add view cart action to snackbars on adding items to cart in main activity

--- app/src/main/java/com/ink/domism/activities/CartActivity.kt
+++ app/src/main/java/com/ink/domism/activities/CartActivity.kt
@@ -1,16 +1,86 @@
 package com.ink.domism.activities
 
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.WindowCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import com.ink.domism.R
 import com.ink.domism.adapters.RecyclerAdapter
 import com.ink.domism.models.CartItem
 import com.ink.domism.models.Item
 import io.paperdb.Paper
+
+class ModalBottomSheet : BottomSheetDialogFragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val view : View =  inflater.inflate(R.layout.modal_bottom_sheet_content, container, false)
+        var itemList : ArrayList<Item> = arrayListOf()
+
+        CartActivity.getCart().forEach {
+            if(it.quantity!=0){
+                itemList += it.product
+            }
+        }
+
+        val adapter = RecyclerAdapter(itemList)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.idrecyclerViewOrder)
+
+        recyclerView.layoutManager = LinearLayoutManager(CartActivity())
+        recyclerView.adapter = adapter
+
+        val totalOGP = view.findViewById<TextView>(R.id.totalOrigPrice)
+        val totalDP = view.findViewById<TextView>(R.id.totalDiscPrice)
+        val totalSP = view.findViewById<TextView>(R.id.totalStorePrice)
+
+        totalOGP.text = "₹" + CartActivity.getTotalOrigPrice().toString()
+        totalOGP.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+        totalOGP.alpha = 0.69F
+        totalDP.text = "₹" + CartActivity.getTotalDiscPrice().toString()
+        totalSP.text = "₹" + CartActivity.getTotalStorePrice().toString()
+
+        val locationMenu = view.findViewById<MaterialAutoCompleteTextView>(R.id.locationMenu)
+        val locations = arrayOf("Location 1", "Location 2", "Location 3", "Location 4")
+        locationMenu.setOnFocusChangeListener { v, hasFocus -> locationMenu.setSimpleItems(locations) }
+        //TODO: Fetch locations from database
+
+        val btnCheckout = view.findViewById<TextView>(R.id.buttonCheckout)
+
+        btnCheckout.append(" (₹" + CartActivity.getTotalDiscPrice().toString()+")")
+        btnCheckout.setOnClickListener {
+            if(locationMenu.text.isNotEmpty()){
+                // do the thang
+            }else{
+                Toast.makeText(it.context, "Please select a location", Toast.LENGTH_SHORT).show()
+            }
+        }
+        adapter.setOnItemClickListener(object: RecyclerAdapter.onItemClickListener{
+            override fun onItemClick(position : Int){
+            }
+        })
+
+        return view
+    }
+
+    companion object{
+        const val TAG ="ModalBottomSheet"
+    }
+}
 
 class CartActivity : AppCompatActivity() {
 
@@ -38,6 +108,14 @@ class CartActivity : AppCompatActivity() {
         mToolbar.setNavigationOnClickListener {
             finish()
         }
+
+        val modalBottomSheet = ModalBottomSheet()
+        //val modalBottomSheetBehavior = (modalBottomSheet.dialog as BottomSheetDialog).behavior
+        val btnPay = findViewById<Button>(R.id.buttonPay)
+        btnPay.setOnClickListener {
+            modalBottomSheet.show(supportFragmentManager, ModalBottomSheet.TAG)
+        }
+
 
         adapter.setOnItemClickListener(object: RecyclerAdapter.onItemClickListener{
             override fun onItemClick(position : Int){
@@ -101,6 +179,30 @@ class CartActivity : AppCompatActivity() {
                 }
             }
             return itemQuantity
+        }
+
+        fun getTotalOrigPrice(): Int{
+            var totalOrigPrice = 0
+            getCart().forEach{
+                totalOrigPrice += it.product.origPrice * it.quantity
+            }
+            return totalOrigPrice
+        }
+
+        fun getTotalDiscPrice(): Int{
+            var totalDiscPrice = 0
+            getCart().forEach{
+                totalDiscPrice += it.product.discPrice * it.quantity
+            }
+            return totalDiscPrice
+        }
+
+        fun getTotalStorePrice(): Int{
+            var totalStorePrice = 0
+            getCart().forEach{
+                totalStorePrice += it.product.storePrice * it.quantity
+            }
+            return totalStorePrice
         }
 
     }
--- app/src/main/java/com/ink/domism/adapters/RecyclerAdapter.kt
+++ app/src/main/java/com/ink/domism/adapters/RecyclerAdapter.kt
@@ -1,12 +1,14 @@
 package com.ink.domism.adapters
 
 import android.app.Activity
+import android.content.Intent
 import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.android.material.snackbar.Snackbar
@@ -30,11 +32,11 @@ class RecyclerAdapter(private val List : ArrayList<Item>):
         val itemName : TextView = itemView.findViewById(R.id.idItemName)
         val origPrice : TextView = itemView.findViewById(R.id.idOrigPrice)
         val discPrice : TextView = itemView.findViewById(R.id.idDiscPrice)
-        val Image: ImageView = itemView.findViewById(R.id.idItemImage)
+        val storePrice : TextView? = itemView.findViewById(R.id.idStorePrice)
+        val Image: ImageView? = itemView.findViewById(R.id.idItemImage)
         val count : TextView = itemView.findViewById(R.id.idItemCount)
-        val btnRemove : ImageView = itemView.findViewById(R.id.remItem)
-        val btnAdd : ImageView = itemView.findViewById(R.id.addItem)
-
+        val btnRemove : ImageView? = itemView.findViewById(R.id.remItem)
+        val btnAdd : ImageView? = itemView.findViewById(R.id.addItem)
         init {
             itemView.setOnClickListener{
                 listener.onItemClick(adapterPosition)
@@ -45,7 +47,15 @@ class RecyclerAdapter(private val List : ArrayList<Item>):
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.card_view, parent, false)
-        return ItemViewHolder(itemView, mListener)
+
+        val itemView2 = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_view_order, parent, false)
+
+        if(parent.context is Activity){
+            return ItemViewHolder(itemView, mListener)
+        }else{
+            return ItemViewHolder(itemView2, mListener)
+        }
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
@@ -56,22 +66,45 @@ class RecyclerAdapter(private val List : ArrayList<Item>):
         holder.origPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
         holder.origPrice.alpha = 0.69F
         holder.discPrice.text = "₹" + List[position].discPrice.toString()
+        holder.storePrice?.text = "₹" + List[position].storePrice.toString()
         holder.count.text = CartActivity.getItemQuantity(item).toString()
 
-        if((holder.itemView.context as Activity).localClassName == "activities.CartActivity"){
-            holder.btnRemove.visibility = View.VISIBLE
-            holder.count.visibility = View.VISIBLE
+        if(holder.itemView.context is Activity) {
+            // Check if it is of type Activity before casting it as one
+            if ((holder.itemView.context as Activity).localClassName == "activities.CartActivity") {
+                holder.btnRemove!!.visibility = View.VISIBLE
+                holder.count.visibility = View.VISIBLE
+                //View Cart Action shouldn't be present in Cart Activity itself
+                holder.btnAdd?.setOnClickListener {
+                    CartActivity.addItem(item)
+                    Snackbar.make(it, item.product.name + " added to your cart!", Snackbar.LENGTH_SHORT)
+                        .setAnchorView(R.id.buttonPay)
+                        .show()
+                    holder.count.text = CartActivity.getItemQuantity(item).toString()
+                }
+            }else{
+                holder.btnAdd?.setOnClickListener {
+                    CartActivity.addItem(item)
+                    Snackbar.make(it, item.product.name + " added to your cart!", Snackbar.LENGTH_SHORT)
+                        .setAction("View Cart") {
+                            startActivity(it.context, Intent(it.context, CartActivity::class.java), null)
+                        }.show()
+                    holder.count.text = CartActivity.getItemQuantity(item).toString()
+                }
+            }
+            // ImageView is not there in ModalBottomSheet
+            Glide
+                .with(holder.Image!!)
+                .load(List[position].image)
+                .placeholder(R.drawable.ic_round_fastfood_24)
+                .centerCrop()
+                .into(holder.Image)
         }
-
-        holder.btnAdd.setOnClickListener {
-            CartActivity.addItem(item)
-            Snackbar.make(it, item.product.name + " added to your cart!", Snackbar.LENGTH_SHORT).show()
-            holder.count.text = CartActivity.getItemQuantity(item).toString()
-        }
-
-        holder.btnRemove.setOnClickListener {
+        holder.btnRemove?.setOnClickListener {
             CartActivity.removeItem(item)
-            Snackbar.make(it, item.product.name + " removed from your cart.", Snackbar.LENGTH_SHORT).show()
+            Snackbar.make(it, item.product.name + " removed from your cart.", Snackbar.LENGTH_SHORT)
+                .setAnchorView(R.id.buttonPay)
+                .show()
             holder.count.text = CartActivity.getItemQuantity(item).toString()
             if(CartActivity.getItemQuantity(item)==0){
                 List.remove<Item>(item.product)
@@ -79,13 +112,6 @@ class RecyclerAdapter(private val List : ArrayList<Item>):
                 notifyItemRangeChanged(position, itemCount - position)
             }
         }
-
-        Glide
-            .with(holder.Image)
-            .load(List[position].image)
-            .placeholder(R.drawable.ic_round_fastfood_24)
-            .centerCrop()
-            .into(holder.Image)
     }
 
     override fun getItemCount(): Int {
